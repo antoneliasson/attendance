bf2de5fa608a3982ee921ad883e48d579ee4d261
Anton Eliasson <devel@antoneliasson.se>
2015-02-15 09:36:39 +0100

    Focus search field on ESC key press

--- src/main/java/se/antoneliasson/attendance/gui/FilterField.java
+++ src/main/java/se/antoneliasson/attendance/gui/FilterField.java
@@ -24,6 +24,7 @@ public class FilterField extends JTextField {
     class CancelAction extends AbstractAction {
         @Override
         public void actionPerformed(ActionEvent ev) {
+            requestFocusInWindow();
             setText("");
         }
     }
